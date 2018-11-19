9d94495902f3c6b3b6f4cc06e915ee5de902e673
Ezekiel Mangalu <emangalu@jirnexu.com>
2018-11-19 14:22:14 +0800

    Test store actions

--- src/store/actions.spec.js
+++ src/store/actions.spec.js
@@ -4,11 +4,19 @@ import actions from  './actions'
 
 Vue.use(Vuex)
 
+const data = [
+  { title: 'one', id: '1' },
+  { title: 'two', id: '2' },
+  { title: 'three', id: '3' }
+]
+
 describe('Actions', () =>  {
   let store
-  let mockSetLemons = jest.fn()
+  let mockSetLemons
 
   beforeEach(() => {
+    mockSetLemons = jest.fn()
+
     store = new Vuex.Store({
       mutations:  {
         setLemons: mockSetLemons
@@ -20,7 +28,29 @@ describe('Actions', () =>  {
   })
 
   it('should fetch lemons', () =>  {
-    store.dispatch('fetchLemons')
-    expect(mockSetLemons).toHaveBeenCalled()
+    const url = './static/lemons.json'
+
+    // Mock fetch
+    global.fetch = jest.fn().mockImplementationOnce(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve({
+        data: JSON.stringify(data)
+      })
+    }))
+
+    return store.dispatch('fetchLemons')
+      .then((res) => {
+        expect(global.fetch).toHaveBeenCalledWith(url)
+        // expect(mockSetLemons).toHaveBeenCalledWith(data)
+      })
+  })
+
+  it('should handle error on failure', () => {
+    global.fetch = jest.fn().mockImplementationOnce(() => Promise.reject('Server error'))
+    return store.dispatch('fetchLemons')
+      .then((res) => {
+        expect(global.fetch).toHaveBeenCalled()
+        expect(mockSetLemons).not.toHaveBeenCalled()
+      })
   })
 }) 
